3b76267973df1e1483aef580707c7be1c9b39a91
unsupplatform <92132009+unsupplatform@users.noreply.github.com>
2026-01-23 19:20:06 +0000

    Update deepwork to v0.4.0

--- Formula/deepwork.rb
+++ Formula/deepwork.rb
@@ -3,8 +3,8 @@ class Deepwork < Formula
 
   desc "Framework for enabling AI agents to perform complex, multi-step work tasks"
   homepage "https://github.com/Unsupervisedcom/deepwork"
-  url "https://files.pythonhosted.org/packages/f9/dd/f0d7f6aaa535869401cc1de714ec88f7a025f43bb32a1cc352c073ff4a2c/deepwork-0.3.1.tar.gz"
-  sha256 "04bee2459d992ea424328bfb6883c241b58637a19a9650ca2f561f492c718287"
+  url "https://files.pythonhosted.org/packages/9c/f7/85d9e3f58e870d718a958b03ba02bd8055666a6dfdaefaa2b9554d4f761d/deepwork-0.4.0.tar.gz"
+  sha256 "93db14d4193c6b14ca040c077d0ef5c0f049f552cc30e7b3e071f7cc88b5e932"
   license "BSL-1.1"
 
   depends_on "python@3.11"
